bc4dc9d894fdce68c8af122f3bc33f1bbb4daaa0
Tomi B <107493451+oluwatomib95@users.noreply.github.com>
2022-06-15 10:18:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+'Cookies and Pancake'
