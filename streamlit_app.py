190e58cbe8ce3909440ddd11cca337d6dd05adfb
Tomi B <107493451+oluwatomib95@users.noreply.github.com>
2022-06-22 09:20:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,6 @@ streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
